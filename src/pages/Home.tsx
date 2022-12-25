f839cdb6fb23f7d7963d72ce017c2614dc26967c
chirag-ghosh <cghosh828049@gmail.com>
2022-12-25 11:50:05 +0530

    search working;

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import Fuse from 'fuse.js';
+import { useEffect, useState } from 'react';
+import links from '../data/saved_links.json';
+
 function Home() {
-  return <div className="home">This website is a work in progress.</div>;
+  const fuse = new Fuse(links.links);
+
+  const [query, setQuery] = useState<string>('');
+  const [linkList, setLinkList] = useState<any[]>([]);
+
+  useEffect(() => {
+    setLinkList(fuse.search(query));
+  }, [query]);
+
+  return (
+    <div className="home">
+      <h1>Search your history up!</h1>
+      <input value={query} type="text" onChange={(e) => setQuery(e.target.value)} />
+      <div className="results">
+        {linkList.map((link) => {
+          return <div key={link.refIndex}>{link.item}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
